78638d8ec6f310bb4b48c529fe6e32abf4e4e3f7
Kevin <kevinu774@gmail.com>
2023-08-22 02:54:59 +0530

    Home and Tours Page Added

---
+++ trip-planner/src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div>
+      {/* <div className='footer_image'>
+        <img src="https://wanderon.in/assets/images/phone-footer.svg" alt="Footer Image" />
+      </div> */}
+      <footer>
+        <div className='footer_info'>
+
+              <div className='footer_logo'>
+                <img src="https://i.ibb.co/6vB9Nhj/Untitled-design-1.png" alt=""  width="140px" />
+              </div>
+              <div>
+                <ul className='footer_list'>
+                  <li className='footer_item'><Link to="/tours">Tours</Link></li>
+                  <li className='footer_item'><Link to="stories">Stories</Link></li>
+                  <li className='footer_item'><Link>Contact Us</Link></li>
+                  <li className='footer_item'><Link>Privacy Policy</Link></li>
+
+                </ul>
+              </div>
+        </div>
+
+
+        <div className="footer_gallery">
+
+                  <img src="https://i.ibb.co/0rSQdsh/Gallery-2.jpg" alt="" width="400px"/>
+            <div className='footer_gallery_images'>
+                  <img src="https://i.ibb.co/Z1Dsxfv/Gallery-3.jpg" alt="" width="250px"/>
+                  <img src="https://i.ibb.co/CsWTK39/Gallery-1.jpg" alt="" width="150px" />
+            </div>
+        </div>
+      </footer>
+      
+    </div>
+  )
+}
+
+export default Footer
